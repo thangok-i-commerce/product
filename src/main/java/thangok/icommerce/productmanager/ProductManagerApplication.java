7b586652f6f77faff01e217c98743160c635b962
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-10 01:00:51 +0700

    add eureka

--- src/main/java/thangok/icommerce/productmanager/ProductManagerApplication.java
+++ src/main/java/thangok/icommerce/productmanager/ProductManagerApplication.java
@@ -2,10 +2,12 @@ package thangok.icommerce.productmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableEurekaClient
 public class ProductManagerApplication {
 
 	public static void main(String[] args) {
